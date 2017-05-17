5dea547c68981c5eb1216d30255352f30ce9e249
laijingfeng <337292616@qq.com>
2017-05-17 20:44:19 +0800

    update

---
+++ Assets/Editor/BuildGoogleAndroid.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class BuildGoogleAndroid : Editor
+{
+    [MenuItem("Tools/BuildGoogleProject")]
+    static public void BuildAssetBundles()
+    {
+        BuildPipeline.BuildPlayer(new string[] { "Assets/Main.unity" }, Application.dataPath + "/../", BuildTarget.Android, BuildOptions.AcceptExternalModificationsToPlayer);
+    }
+}
